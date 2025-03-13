de0fe22534374f76bbb8f28d91551e47697213b3
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2025-03-13 21:05:16 +0530

    test workflow

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -5,11 +5,7 @@ import { upload } from "../middlewares/multer.middleware.js";
 
 const questionRouter = Router();
 
-questionRouter.get(
-  "/questions",
-  authenticateUser,
-  QuestionController.getAllQuestions,
-);
+questionRouter.get("/questions", QuestionController.getAllQuestions);
 
 questionRouter.post(
   "/questions/media/upload",
