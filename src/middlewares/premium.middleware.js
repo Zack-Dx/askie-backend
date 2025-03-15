23db86fdb0518446bf16ede5206b1a12925698f2
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2025-03-15 15:37:13 +0530

    premium middleware added.

---
+++ src/middlewares/premium.middleware.js
@@ -0,0 +1,12 @@
+import { formatApiResponse } from "../utils/helper";
+
+export const checkUserPremium = (req, res, next) => {
+  const user = req.user;
+  if (user.isPremium) {
+    return next();
+  }
+
+  return res
+    .status(400)
+    .json(formatApiResponse(400, false, null, "Premium Access Only"));
+};
