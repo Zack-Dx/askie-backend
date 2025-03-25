cb299e31ba8ec0bcad24dd639a4b83a348680bbd
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2025-03-25 17:08:17 +0530

    middleware updated.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,8 @@ import {
 
 async function authenticateUser(req, res, next) {
   try {
-    const { token } = req.cookies;
+    const token =
+      req.headers.authorization?.split(" ")[1] || req.cookies?.token;
 
     if (!token) {
       return res
@@ -53,6 +54,7 @@ async function authenticateUser(req, res, next) {
       ...user,
       accountAge: calculateAccountAgeInDays(user.createdAt),
     };
+
     next();
   } catch (error) {
     console.error("Authentication error:", error);
